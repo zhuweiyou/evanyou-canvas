e794694b6c132b3de1c9f1a7c946d9950540b96e
zhuweiyou <i@zhuweiyou.com>
2020-04-26 10:47:35 +0800

    add

---
+++ README.md
@@ -0,0 +1,13 @@
+# evanyou-canvas
+
+## API
+
+```js
+const evanyouCanvas = require("evanyou-canvas");
+
+const parentElement = document.querySelector("#parent");
+const { element, update } = evanyouCanvas(parentElement);
+
+// element: canvas element
+// update(): draw canvas
+```
---
+++ src/index.js
@@ -0,0 +1,77 @@
+export default function evanyouCanvas(parentElement, documentClick = true) {
+  const element = create(parentElement);
+  const update = drawCanvas(element);
+  if (documentClick) {
+    document.addEventListener("click", update);
+  }
+  return {
+    element,
+    update,
+  };
+}
+
+function create(parentElement) {
+  const element = document.createElement("canvas");
+  element.setAttribute(
+    "style",
+    `position:absolute;left:0;top:0;width:100%;height:100%;z-index:-1;pointer-events:none`
+  );
+  parentElement.prependChild(element);
+  return element;
+}
+
+function drawCanvas(c) {
+  let x = c.getContext("2d"),
+    pr = window.devicePixelRatio || 1,
+    w = window.innerWidth,
+    h = window.innerHeight,
+    f = 90,
+    q,
+    m = Math,
+    r = 0,
+    u = m.PI * 2,
+    v = m.cos,
+    z = m.random;
+  c.width = w * pr;
+  c.height = h * pr;
+  x.scale(pr, pr);
+  x.globalAlpha = 0.6;
+
+  function i() {
+    x.clearRect(0, 0, w, h);
+    q = [
+      { x: 0, y: h * 0.7 + f },
+      { x: 0, y: h * 0.7 - f },
+    ];
+    while (q[1].x < w + f) d(q[0], q[1]);
+  }
+
+  function d(i, j) {
+    x.beginPath();
+    x.moveTo(i.x, i.y);
+    x.lineTo(j.x, j.y);
+    var k = j.x + (z() * 2 - 0.25) * f,
+      n = y(j.y);
+    x.lineTo(k, n);
+    x.closePath();
+    r -= u / -50;
+    x.fillStyle =
+      "#" +
+      (
+        ((v(r) * 127 + 128) << 16) |
+        ((v(r + u / 3) * 127 + 128) << 8) |
+        (v(r + (u / 3) * 2) * 127 + 128)
+      ).toString(16);
+    x.fill();
+    q[0] = q[1];
+    q[1] = { x: k, y: n };
+  }
+
+  function y(p) {
+    const t = p + (z() * 2 - 1.1) * f;
+    return t > h || t < 0 ? y(p) : t;
+  }
+
+  i();
+  return i;
+}
